e0395f2cbff05791f478f3189702b690734aa99e
Petter Jerndal <jerndal.petter@gmail.com>
2020-02-04 14:49:57 +0100

    Task 4-7 completed.

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -84,3 +84,65 @@ function menuItem(name, kCal, gluten, veg, img) {
     };
 }
 */
+
+/*
+let burg1 = new menuItem('La Foledoledoledoff', 870, true, false, 'img/umami.jpg');
+
+let burg2 = new menuItem('Sneskus', 960, true, true, 'img/snuskis.jpg');
+
+let burg3 = new menuItem('Le Monde', 740, true, false, 'img/dag.jpg');
+
+let burg4 = new menuItem('Tango', 900, true, true, 'img/halloumi.jpg');
+
+let burg5 = new menuItem('Winter Cafe', 920, true, false, 'img/fetmaxburgare.png');
+/*
+const vm = new Vue({
+  el: '#myID',
+  data: {
+    //arbitraryVariableName: 'Välj en burgare ' + new Date()
+    
+    b1: burg1.name(),
+    b2: burg2.name(),
+    b3: burg3.name(),
+    b4: burg4.name(),
+    b5: burg5.name()
+
+
+  }
+})
+*/
+/*
+var menu = [burg1, burg2, burg3, burg4, burg5];
+
+let myElement = document.getElementById("myID");
+
+for (i=0; i<menu.length; i++)
+{
+        let listItem = document.createElement("li");
+        let listValue = document.createTextNode(menu[i].name());
+        listItem.appendChild(listValue);
+        myElement.appendChild(listItem);
+}
+
+function menuItem(name, kCal, gluten, veg, img) {
+    this.firstName = name; 
+    this.calories = kCal;
+    this.gluten = gluten;
+    this.vegetarian = veg;
+    this.image = img;
+    this.name = function() {
+    var name = this.firstName + ' ' + this.calories;
+    var gluten = '';
+    var veg = '';
+        if (this.gluten){
+        	gluten = ' gluten ';
+        }
+        if(this.vegetarian){
+        	veg = 'vegetarian'; 
+        
+        }
+  	      
+	 	return name + gluten + veg;
+
+    };
+*/
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -30,7 +30,7 @@ const vm = new Vue({
   }
 })
 
-function menuItem(name, kCal, gluten, veg, img) {
+function menuItem(name, kCal, gluten, veg, img) { 
     this.firstName = name; 
     this.calories = kCal;
     this.gluten = gluten;
@@ -40,3 +40,42 @@ function menuItem(name, kCal, gluten, veg, img) {
         return this.firstName + ' ' + this.calories;
     };
 }
+
+/*
+let burg1 = new menuItem('La Foledoledoledoff', 870, true, false, 'img/umami.jpg');
+
+let burg2 = new menuItem('Sneskus', 960, true, true, 'img/snuskis.jpg');
+
+let burg3 = new menuItem('Le Monde', 740, true, false, 'img/dag.jpg');
+
+let burg4 = new menuItem('Tango', 900, true, true, 'img/halloumi.jpg');
+
+let burg5 = new menuItem('Winter Cafe', 920, true, false, 'img/fetmaxburgare.png');
+
+const vm = new Vue({
+  el: '#myID',
+  data: {
+    //arbitraryVariableName: 'Välj en burgare ' + new Date()
+    			burgers: [
+          	burg1,
+            burg2,
+            burg3, 
+            burg4,
+            burg5
+          
+          ]
+  }
+})
+
+function menuItem(name, kCal, gluten, veg, img) {
+    this.firstName = name; // The this keyword refers to the object itself
+    this.calories = kCal;
+    this.gluten = gluten;
+    this.vegetarian = veg;
+    this.image = img;
+    this.name = function() {
+        return this.firstName + ' ' + this.calories;
+    };
+}
+
+*/
